1e7c9aade38f6fdac64f81a4783be3828149aa5b
ernestorubens98 <ernestorubens13@gmail.com>
2020-06-17 15:31:16 -0300

    Exercicios do modulo iniciado em Fron-End concluido

---
+++ desafios/02-Ex1-Ex2-Ex3/2-3scripts.js
@@ -0,0 +1,18 @@
+const modalOverlay = document.querySelector('.modal-overlay');
+const cursos = document.querySelectorAll('.curso');
+const buttonClose = document.querySelector('.close-modal')
+
+for (let curso of cursos) {
+    curso.addEventListener('click', () => {
+        modalOverlay.classList.add('active')
+
+        const videoId = curso.getAttribute('id')
+        modalOverlay.querySelector('iframe').src = `https://rocketseat.com.br/${videoId}`
+    })
+}
+
+buttonClose.addEventListener('click', () => {
+    modalOverlay.classList.remove('active')
+
+    modalOverlay.querySelector('iframe').src = '';
+})
